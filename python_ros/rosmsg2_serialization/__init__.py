e1a3d1b618b386d0df10310224c6f44312ec1856
Daisuke Kato <kato.daisuke429@gmail.com>
2025-08-11 01:25:52 +0900

    feat: port rosmsg2 serialization to python (#9)
    * feat: port rosmsg2 serialization to python
    
    * feat: parse ros2 idl definitions
    
    * test: split rosmsg2 serialization tests

---
+++ python_ros/rosmsg2_serialization/__init__.py
@@ -0,0 +1,4 @@
+from .MessageReader import MessageReader
+from .MessageWriter import MessageWriter
+
+__all__ = ["MessageReader", "MessageWriter"]
---
+++ python_ros/tests/test_MessageWriter.py
@@ -0,0 +1,166 @@
+import struct
+
+import pytest
+from rosmsg2_serialization import MessageWriter
+from rosmsg.parse import parse
+
+
+def _serialize_string(string: str) -> bytes:
+    data = string.encode("utf8")
+    return struct.pack("<I", len(data) + 1) + data + b"\x00"
+
+
+def test_primitive_fields():
+    msg_def = """int32 a
+string b
+"""
+    defs = parse(msg_def, ros2=True)
+    writer = MessageWriter(defs)
+    message = {"a": -42, "b": "hello"}
+    expected = b"\x00\x01\x00\x00" + struct.pack("<i", -42) + _serialize_string("hello")
+    assert writer.write_message(message) == expected
+
+
+def test_array_field():
+    msg_def = """int32[] values
+"""
+    defs = parse(msg_def, ros2=True)
+    writer = MessageWriter(defs)
+    message = {"values": [1, 2, 3]}
+    expected = b"\x00\x01\x00\x00" + struct.pack("<I", 3) + struct.pack("<3i", 1, 2, 3)
+    assert writer.write_message(message) == expected
+
+
+def test_nested_time():
+    msg_def = """builtin_interfaces/msg/Time stamp
+================================================================================
+MSG: builtin_interfaces/msg/Time
+int32 sec
+uint32 nanosec
+"""
+    defs = parse(msg_def, ros2=True)
+    writer = MessageWriter(defs)
+    message = {"stamp": {"sec": 1, "nanosec": 2}}
+    expected = b"\x00\x01\x00\x00" + struct.pack("<iI", 1, 2)
+    assert writer.write_message(message) == expected
+
+
+def test_empty_message():
+    msg_def = ""
+    defs = parse(msg_def, ros2=True)
+    writer = MessageWriter(defs)
+    assert writer.write_message({}) == b"\x00\x01\x00\x00\x00"
+
+
+@pytest.mark.skip("ros2idl parser support pending")
+def test_ros2idl_tf_message():
+    from ros2idl_parser import parse_ros2idl
+
+    buffer = bytes.fromhex(
+        "0001000001000000286fae6169ddd73108000000747572746c6531000e000000"
+        "747572746c65315f616865616400000000000000000000000000f03f00000000"
+        "0000000000000000000000000000000000000000000000000000000000000000"
+        "00000000000000000000f03f"
+    )
+    msg_def = """
+================================================================================
+IDL: geometry_msgs/msg/Transforms
+
+module geometry_msgs {
+  module msg {
+    struct Transforms {
+      sequence<geometry_msgs::msg::TransformStamped> transforms;
+    };
+  };
+};
+================================================================================
+IDL: geometry_msgs/msg/TransformStamped
+
+module geometry_msgs {
+  module msg {
+    struct TransformStamped {
+      std_msgs::msg::Header header;
+      string child_frame_id; // the frame id of the child frame
+      geometry_msgs::msg::Transform transform;
+    };
+  };
+};
+================================================================================
+IDL: std_msgs/msg/Header
+
+module std_msgs {
+  module msg {
+    struct Header {
+      builtin_interfaces::Time stamp;
+      string frame_id;
+    };
+  };
+};
+================================================================================
+IDL: geometry_msgs/msg/Transform
+
+module geometry_msgs {
+  module msg {
+    struct Transform {
+      geometry_msgs::msg::Vector3 translation;
+      geometry_msgs::msg::Quaternion rotation;
+    };
+  };
+};
+
+================================================================================
+IDL: geometry_msgs/msg/Vector3
+
+module geometry_msgs {
+  module msg {
+    struct Vector3 {
+      double x;
+      double y;
+      double z;
+    };
+  };
+};
+
+================================================================================
+IDL: geometry_msgs/msg/Quaternion
+
+module geometry_msgs {
+  module msg {
+    struct Quaternion {
+      double x;
+      double y;
+      double z;
+      double w;
+    };
+  };
+};
+
+================================================================================
+IDL: builtin_interfaces/Time
+// Normally added when generating idl schemas
+
+module builtin_interfaces {
+  struct Time {
+    int32 sec;
+    uint32 nsec;
+  };
+};
+    """
+    defs = parse_ros2idl(msg_def)
+    writer = MessageWriter(defs)
+    message = {
+        "transforms": [
+            {
+                "header": {
+                    "stamp": {"sec": 1638821672, "nsec": 836230505},
+                    "frame_id": "turtle1",
+                },
+                "child_frame_id": "turtle1_ahead",
+                "transform": {
+                    "translation": {"x": 1.0, "y": 0.0, "z": 0.0},
+                    "rotation": {"x": 0.0, "y": 0.0, "z": 0.0, "w": 1.0},
+                },
+            }
+        ]
+    }
+    assert writer.write_message(message) == buffer
